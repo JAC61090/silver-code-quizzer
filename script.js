dbb79ac4356853fc4546c0650764c122b0474403
Jason carrazco <carrazcojason10@gmail.com>
2021-06-02 18:31:53 -0500

    setting function that cycles through object array

--- script.js
+++ script.js
@@ -80,3 +80,18 @@ var timerInterval;
 var score = 0;
 var correct;
 
+// This function cycles through the object array containing the quiz questions to generate the questions and answers.
+function generateQuizQuestion(){
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+
