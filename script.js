7c9cb932a298004e688a8ea512b6385572e8f0d2
Jason carrazco <carrazcojason10@gmail.com>
2021-06-02 18:15:08 -0500

    setting js elements for html DOM

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Gathering HTML elements for manipulation
+// HTML elements to manipulate
 var quizBody = document.getElementById("quiz");
 var resultsEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("finalScore");
@@ -8,8 +8,12 @@ var quizTimer = document.getElementById("timer");
 var startQuizButton = document.getElementById("startbtn");
 var startQuizDiv = document.getElementById("startpage");
 var highscoreContainer = document.getElementById("highscoreContainer");
-
-
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
