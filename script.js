9834480b274f731702031f366f2e7297c56045f8
Jason carrazco <carrazcojason10@gmail.com>
2021-06-02 18:06:40 -0500

    html gameover divs/html elem in scipt for manipulation

---
+++ script.js
@@ -0,0 +1,10 @@
+// Gathering HTML elements for manipulation
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
